365ec1006a4940eb8fe7f0ab8225ddccc9b6291c
KearChen <kearchen@qq.com>
2025-04-28 21:07:22 +0800

    增加运行模式配置

--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"fmt"
 	"gin_jiaoshoujia/config"
-	"gin_jiaoshoujia/models"
 	"gin_jiaoshoujia/routers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 初始化配置
+	// ------------------------- 配置初始化 -------------------------
+	// 初始化全局配置文件
 	config.InitConfig()
 
-	// 初始化数据库
-	models.InitDB()
+	// 设置 Gin 运行模式
+	mode := config.Config.GetString("app.SetMode")
+	gin.SetMode(mode)
 
-	// 初始化路由
+	// ------------------------- 路由初始化 -------------------------
+	// 初始化 Gin 路由
 	app := routers.InitRouter()
 
-	// 启动服务器
+	// ------------------------- 服务启动 -------------------------
+	// 获取服务监听端口
 	port := config.Config.GetInt("app.port")
+	fmt.Printf("服务启动，端口：%d\n", port)
+
+	// 启动 HTTP 服务
 	err := app.Run(fmt.Sprintf(":%d", port))
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("服务启动失败: %v", err))
 	}
 }
